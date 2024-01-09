1cb9ae867c991c23a682e6d79005f60a9f713b1d
avigarg24 <154696013+avigarg24@users.noreply.github.com>
2024-01-10 01:21:08 +0530

    Update YourSensitiveData.swift

--- SpotifyCloneProject/YourSensitiveData.swift
+++ SpotifyCloneProject/YourSensitiveData.swift
@@ -1,4 +1,4 @@
 struct YourSensitiveData {
-  static let clientID = <YOUR_ID>
-  static let clientSecret = <YOUR_SECRET>
+  static let clientID = 786bc62a43344a109f1d16b073b80bbc
+  static let clientSecret = faff21f033984d78a5347c65f7556930
 }
